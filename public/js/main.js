93b405e8f1901e44c48829842aa2dfb334840bfc
Mark Lusby <mlusby@gmail.com>
2013-09-26 20:26:20 -0500

    Changed styles, added constraints

--- public/js/main.js
+++ public/js/main.js
@@ -1,1 +1,4 @@
-$(function(){$(".robot").draggable();});
+$(function(){$(".blue-robot").draggable({ containment: "#row1", scroll: false });});
+$(function(){$(".red-robot").draggable({ containment: "#row2", scroll: false });});
+$(function(){$(".yellow-robot").draggable({ containment: "#row4", scroll: false });});
+$(function(){$(".green-robot").draggable({ containment: "#row4", scroll: false });});
