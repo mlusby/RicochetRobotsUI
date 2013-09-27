89f08b97d402b55d92dddbe26f6785f44fb19d0e
Mark Lusby <mlusby@gmail.com>
2013-09-26 20:28:01 -0500

    Snap to grid

--- public/js/main.js
+++ public/js/main.js
@@ -1,4 +1,4 @@
-$(function(){$(".blue-robot").draggable({ containment: "#row1", scroll: false });});
-$(function(){$(".red-robot").draggable({ containment: "#row2", scroll: false });});
-$(function(){$(".yellow-robot").draggable({ containment: "#row4", scroll: false });});
-$(function(){$(".green-robot").draggable({ containment: "#row4", scroll: false });});
+$(function(){$(".blue-robot").draggable({ containment: "#row1", scroll: false, grid: [34,34] });});
+$(function(){$(".red-robot").draggable({ containment: "#row2", scroll: false, grid: [34,34] });});
+$(function(){$(".yellow-robot").draggable({ containment: "#row4", scroll: false, grid: [34,34] });});
+$(function(){$(".green-robot").draggable({ containment: "#row4", scroll: false, grid: [34,34] });});
