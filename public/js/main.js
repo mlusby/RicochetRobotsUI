174e388eb107b855c1ffbbba1b1574def653e9ed
Mark Lusby <mlusby@gmail.com>
2015-04-21 19:03:44 -0500

    Moving towards data driven board layout
    Not sure what's going on.

--- public/js/main.js
+++ public/js/main.js
@@ -20,13 +20,18 @@ var boardLayout = {
 	blueRobot : [3,1],
 	redRobot : [1,2],
 	yellowRobot : [3,4],
-	greenRobot : [5,6]
+	greenRobot : [5,6],
+	board : [9,1,1,1,1,1,1,1,1,1,1,1,1,1,1,3]
 };
 var initBoard = function (board) {
 	setRobotLocation($("#blue-robot"),board.blueRobot);
 	setRobotLocation($("#red-robot"),board.redRobot);
 	setRobotLocation($("#yellow-robot"),board.yellowRobot);
 	setRobotLocation($("#green-robot"),board.greenRobot);
+	for (var i = 0 ; i < board.board.length ; i++){
+		var binary = board.board[i].toString(2);
+		if (binary & 1) { $("#game-board #row1 .box").css("border-top","2px solid black"); }
+	}
 };
 var setRobotLocation = function(robot, coords) {
 	var h = (coords[0] * 34) + 5,
