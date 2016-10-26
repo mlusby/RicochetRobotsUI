8fe5508108179ac7d493f791237fc30f9a61aecc
Ubuntu <mlusby@gmail.com>
2016-10-26 01:29:48 +0000

    Removed keys, fixed spacing

--- web.js
+++ web.js
@@ -7,9 +7,7 @@ app.use(express.static('public'));
 
 var hbs = require('hbs');   //Hooray Handlebars!
 AWS.config.update({
-        accessKeyId: "AccessKeyId",
-        secretAccessKey: "SecretAccessKey",
-        region: "us-west-2"
+    region: "us-west-2"
 });
 var docClient = new AWS.DynamoDB.DocumentClient();
 
@@ -19,21 +17,19 @@ app.use(express.bodyParser());
 
 
 app.get('/', function(req, res) {
-
-        
-        var table = "Puzzles";
-            var key = "2dc9e6432ce8b9c159120b4e178dad01859ad428";
-                var params = {};
-                    params.TableName = table;
-                        params.Key = {};
-                            params.Key.key = key;
-                                docClient.get(params, function(err, data) {
-                                            if (err) {
-                                                                    console.error("Unable to read item. Error JSON:", JSON.stringify(err, null, 2));
-                                                                                            } else {
-                                                                                                   res.render('index', {'puzzle': JSON.stringify(data, null, 2)});
-                                                                                                                                           }
-                                                });
+    var table = "Puzzles";
+    var key = "2dc9e6432ce8b9c159120b4e178dad01859ad428";
+    var params = {};
+    params.TableName = table;
+    params.Key = {};
+    params.Key.key = key;
+    docClient.get(params, function(err, data) {
+        if (err) {
+            console.error("Unable to read item. Error JSON:", JSON.stringify(err, null, 2));
+        } else {
+           res.render('index', {'puzzle': JSON.stringify(data, null, 2)});
+        }
+    });
 });
 
 app.get('/test/:id', function(req, res) {
